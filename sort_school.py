4932088684496d476b0fded7f2a844450ce14232
mwamba <mwambajoakim@gmail.com>
2024-07-18 18:11:15 +0300

    Sorts different ages to levels of schools

---
+++ sort_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+age = eval(input("Enter your age: "))
+if (age == 5):
+    print("Go to Kindergarten!")
+elif(age >= 6 and age <= 17):
+    print("Go to Grades 1 through to 12!")
+elif(age >= 17 and age <= 25):
+    print("Go to College!")
+else:
+    print("You are done with school!")
