3336e8cf6902615d8d0bd4abde96218bd4471944
mwamba <mwambajoakim@gmail.com>
2024-07-18 19:15:28 +0300

    Remove whitespace

--- sort_school.py
+++ sort_school.py
@@ -2,9 +2,9 @@
 age = eval(input("Enter your age: "))
 if (age == 5):
     print("Go to Kindergarten!")
-elif(age >= 6 and age <= 17):
+elif (age >= 6 and age <= 17):
     print("Go to Grades 1 through to 12!")
-elif(age >= 17 and age <= 25):
+elif (age >= 17 and age <= 25):
     print("Go to College!")
 else:
     print("You are done with school!")
