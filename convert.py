86c939a97e386a60469b4ad07dc902eced102c7b
mwamba <mwambajoakim@gmail.com>
2024-07-22 13:18:25 +0300

    Add a loop

--- convert.py
+++ convert.py
@@ -6,11 +6,12 @@
 
 def main():
     print("""This program will convert temperature to farenheit.
-    It will ask you for input as celsius.""")
-    celsius = input("What is the temperature in Celsius? ")
-    celsius = float(celsius)
-    farenheit = 9/5 * celsius + 32
-    print(f"The temperature in farenheit is {farenheit}")
+        It will ask you for input as celsius.""")
+    for i in range(5):
+        celsius = input("What is the temperature in Celsius? ")
+        celsius = float(celsius)
+        farenheit = 9/5 * celsius + 32
+        print(f"The temperature in farenheit is {farenheit}")
     input("Press the <Enter> key to quit")
 
 
